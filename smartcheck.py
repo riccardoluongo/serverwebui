2ffa5515fc9b1543003519290fa6598f9f5bfe57
riccardoluongo <ildio4nubi@gmail.com>
2025-05-28 00:01:07 +0200

    add warning for non-zero exit code

--- smartcheck.py
+++ smartcheck.py
@@ -16,13 +16,15 @@ def smartcheck(drive):
 
     attr_json = run(f'/usr/sbin/smartctl -a {drive} --json', shell=True, text=True, stdout=PIPE, stderr=STDOUT)
     if attr_json.returncode == 1:
-        main.app.logger.error(f"Couldn't retrieve SMART data for drive {drive}: {e}")
+        main.app.logger.error(f"Couldn't retrieve SMART data for drive {drive}: {attr_json.stdout}")
         return
+    elif attr_json.returncode > 1:
+        main.app.logger.warning(f"smartctl exited with non-zero exit code: {attr_json.returncode}")
 
     attributes = json.loads(attr_json.stdout)
 
     if 'nvme' in drive:
-        return ('nvme',attributes['nvme_smart_health_information_log'])
+        return ('nvme',attributes['nvme_smart_health_information_log'], attr_json.returncode)
     else:
-        return ('ata',attributes['ata_smart_attributes'])
-#by Riccardo Luongo, 21/05/2025
+        return ('ata',attributes['ata_smart_attributes'], attr_json.returncode)
+#by Riccardo Luongo, 27/05/2025
