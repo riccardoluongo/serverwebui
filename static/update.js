6b292ead5bd95e42946162a5584638b0eb2c4e33
riccardoluongo <ildio4nubi@gmail.com>
2025-05-28 00:02:04 +0200

    add warning message for non-zero smartctl exit code

--- static/update.js
+++ static/update.js
@@ -884,8 +884,12 @@ function showSmart() {
 function updateDiskSelector() {
   const storageBoxWrapper = document.getElementById("storage-box-wrapper");
 
-  const diskSelDiv = storageBoxWrapper.appendChild(document.createElement("div"));
-  const diskSelector = diskSelDiv.appendChild(document.createElement("select"));
+  const diskSelectorWrapper = storageBoxWrapper.appendChild(document.createElement("div"));
+  const diskSelector = diskSelectorWrapper.appendChild(document.createElement("select"));
+  const warningWrapper = diskSelectorWrapper.appendChild(document.createElement("div"));
+
+  diskSelectorWrapper.className = "disk-selector-wrapper";
+  warningWrapper.id = "warning-wrapper";
   setAttributes(diskSelector, {
     id: "disk-selector",
     class: "pool-selector",
@@ -939,6 +943,20 @@ function updateSmartDiv() {
     })
     .then((data) => {
       if (data) {
+        const warningWrapper = document.getElementById("warning-wrapper");
+        removeChildren(warningWrapper);
+
+        if (data[2] != 0) {
+          const warningIcon = warningWrapper.appendChild(document.createElement("svg"));
+          const warningText = warningWrapper.appendChild(document.createElement("span"));
+
+          warningIcon.className = "warning-icon";
+          warningText.className = "warning-text";
+
+          warningIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" viewBox="0 0 512 512"><path fill="#FFD43B" d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480L40 480c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24l0 112c0 13.3 10.7 24 24 24s24-10.7 24-24l0-112c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/></svg>`
+          warningText.innerText = `smartctl returned non-zero exit code: ${data[2]}`;
+        }
+
         if (data[0] == "nvme") {
           const titleRow = smartTable.appendChild(document.createElement("tr"));
 
